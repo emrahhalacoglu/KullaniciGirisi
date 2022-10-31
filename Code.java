97a3943404065b9ce507e3c593ed525576c1c85c
ehm <emrahhalacoglu@gmail.com>
2022-10-31 15:56:40 +0300

    while Loop added.

--- Code.java
+++ Code.java
@@ -3,35 +3,27 @@ public class Code {
     public static void main(String[] args) {
         
         try (Scanner inp = new Scanner(System.in)) {
-            String userName,password,karar,kullaniciAdi,sifre;
+            String userName,Password,kullaniciAdi,sifre;
             kullaniciAdi="patika";
             sifre="JAVA101";
-            System.out.print("User Name : ");
-            userName=inp.nextLine();
-            System.out.print("Password : ");
-            password=inp.nextLine();
+            boolean isUsernameSuccess=false;
+            boolean isPasswordSuccess=false;
             
-            if(userName.equals(kullaniciAdi)){
-                if(password.equals(sifre)){
-                    System.out.println("Giris Basarili");}
-                    else{
-                        System.out.println("Password Hatali!");}
-                }else{
-                    System.out.println("Hatali User Name!");}
-            if(userName!="patika"||password!="JAVA101"){
-                System.out.print("User Name ve Password degistirmek ister misiniz? (Y/N) : ");
-                karar=inp.nextLine();
-                if(karar.equals("Y")){
-                    System.out.print("New User Name : ");
-                    kullaniciAdi=inp.nextLine();
-                    System.out.print("New Password : ");
-                    sifre=inp.nextLine();if(kullaniciAdi.equals("patika")||sifre.equals("JAVA101")){
-                        System.out.println("Kullanici adi ve sifre olusturulamadi!");}else{
-                        System.out.println("islem basarili");}}else{
-                                System.out.println("Tekrar Deneyiniz.");}}
             
-                    }
-        
-    }
 
+            
+            while(!isUsernameSuccess){
+                System.out.print("User Name : ");
+                userName=inp.nextLine();
+                if(userName.equals(kullaniciAdi)){
+                        isUsernameSuccess=true;
+                        while(!isPasswordSuccess){
+                            System.out.print("Password : ");
+                            Password=inp.nextLine();
+                            if(Password.equals(sifre)){System.out.println("Giris Basarili!");isPasswordSuccess=true;}else{System.out.println("Password Hatali!\nTekrar Deneyiniz.\n");}}}
+                    else{
+                        System.out.println("User Name Hatali!\nTekrar Deneyiniz\n");}
+                }
+            }
     }
+}
